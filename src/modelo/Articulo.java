18bd13695935be9561522a99c922b7c61c691f30
Ismael Bastida <ismaelbastida525@gmail.com>
2017-05-23 19:57:22 +0200

    Commit sobre la vista básica
    -Creación de la vista básica, del alta y la consulta de cliente.
    -Creación de algunas clases en modelo
    
    Ismael

---
+++ src/modelo/Articulo.java
@@ -0,0 +1,7 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Articulo implements Serializable {
+
+}
---
+++ src/modelo/Pedido.java
@@ -0,0 +1,7 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Pedido implements Serializable {
+
+}
