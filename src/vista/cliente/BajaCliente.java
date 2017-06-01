99f4d31af4f1820a0b3760119d604c3164dbfa43
Ismael Bastida <ismaelbastida525@gmail.com>
2017-06-01 13:31:24 +0200

    Vistas para Baja y Busqueda de cliente
    Ismael

--- src/vista/cliente/BajaCliente.java
+++ src/vista/cliente/BajaCliente.java
@@ -1,140 +1,35 @@
 package vista.cliente;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.GridBagConstraints;
-import java.awt.Color;
-import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
 import javax.swing.JButton;
-import java.awt.Font;
-		
-		JPanel pnlConsultaCli = new JPanel();
-		pnlConsultaCli.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		pnlConsultaCli.setBackground(Color.WHITE);
-		GridBagConstraints gbc_pnlConsultaCli = new GridBagConstraints();
-		gbc_pnlConsultaCli.fill = GridBagConstraints.BOTH;
-		gbc_pnlConsultaCli.gridx = 0;
-		gbc_pnlConsultaCli.gridy = 0;
-		add(pnlConsultaCli, gbc_pnlConsultaCli);
-		GridBagLayout gbl_pnlConsultaCli = new GridBagLayout();
-		gbl_pnlConsultaCli.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_pnlConsultaCli.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_pnlConsultaCli.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_pnlConsultaCli.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		pnlConsultaCli.setLayout(gbl_pnlConsultaCli);
-		
-		JLabel lblConsulta = new JLabel("Consulta Cliente");
-		GridBagConstraints gbc_lblConsulta = new GridBagConstraints();
-		gbc_lblConsulta.gridwidth = 6;
-		gbc_lblConsulta.insets = new Insets(0, 0, 5, 0);
-		gbc_lblConsulta.gridx = 0;
-		gbc_lblConsulta.gridy = 0;
-		pnlConsultaCli.add(lblConsulta, gbc_lblConsulta);
-		
-		JLabel lblSeleccion = new JLabel("Seleccione Cliente");
-		GridBagConstraints gbc_lblSeleccion = new GridBagConstraints();
-		gbc_lblSeleccion.gridwidth = 2;
-		gbc_lblSeleccion.insets = new Insets(0, 0, 5, 5);
-		gbc_lblSeleccion.gridx = 1;
-		gbc_lblSeleccion.gridy = 1;
-		pnlConsultaCli.add(lblSeleccion, gbc_lblSeleccion);
-		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridwidth = 2;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.gridx = 3;
-		gbc_comboBox.gridy = 1;
-		pnlConsultaCli.add(comboBox, gbc_comboBox);
-		
-		JLabel lblDni = new JLabel("Dni");
-		lblDni.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_lblDni = new GridBagConstraints();
-		gbc_lblDni.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDni.gridx = 2;
-		gbc_lblDni.gridy = 3;
-		pnlConsultaCli.add(lblDni, gbc_lblDni);
-		
-		txtDni = new JTextField();
-		txtDni.setEditable(false);
-		txtDni.setColumns(10);
-		GridBagConstraints gbc_txtDni = new GridBagConstraints();
-		gbc_txtDni.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtDni.insets = new Insets(0, 0, 5, 5);
-		gbc_txtDni.gridx = 4;
-		gbc_txtDni.gridy = 3;
-		pnlConsultaCli.add(txtDni, gbc_txtDni);
-		
-		JLabel lblNombre = new JLabel("Nombre");
-		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
-		gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNombre.gridx = 2;
-		gbc_lblNombre.gridy = 4;
-		pnlConsultaCli.add(lblNombre, gbc_lblNombre);
-		
-		txtNombre = new JTextField();
-		txtNombre.setEditable(false);
-		txtNombre.setColumns(10);
-		GridBagConstraints gbc_txtNombre = new GridBagConstraints();
-		gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtNombre.insets = new Insets(0, 0, 5, 5);
-		gbc_txtNombre.gridx = 4;
-		gbc_txtNombre.gridy = 4;
-		pnlConsultaCli.add(txtNombre, gbc_txtNombre);
-		
-		JLabel lblApellido = new JLabel("Apellidos");
-		GridBagConstraints gbc_lblApellido = new GridBagConstraints();
-		gbc_lblApellido.insets = new Insets(0, 0, 5, 5);
-		gbc_lblApellido.gridx = 2;
-		gbc_lblApellido.gridy = 5;
-		pnlConsultaCli.add(lblApellido, gbc_lblApellido);
-		
-		txtApellido = new JTextField();
-		txtApellido.setEditable(false);
-		txtApellido.setColumns(10);
-		GridBagConstraints gbc_txtApellido = new GridBagConstraints();
-		gbc_txtApellido.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtApellido.insets = new Insets(0, 0, 5, 5);
-		gbc_txtApellido.gridx = 4;
-		gbc_txtApellido.gridy = 5;
-		pnlConsultaCli.add(txtApellido, gbc_txtApellido);
-		
-		JLabel lblDireccion = new JLabel("Direcci\u00F3n");
-		GridBagConstraints gbc_lblDireccion = new GridBagConstraints();
-		gbc_lblDireccion.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDireccion.gridx = 2;
-		gbc_lblDireccion.gridy = 6;
-		pnlConsultaCli.add(lblDireccion, gbc_lblDireccion);
-		
-		txtDireccion = new JTextField();
-		txtDireccion.setEditable(false);
-		txtDireccion.setColumns(10);
-		GridBagConstraints gbc_txtDireccion = new GridBagConstraints();
-		gbc_txtDireccion.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtDireccion.insets = new Insets(0, 0, 5, 5);
-		gbc_txtDireccion.gridx = 4;
-		gbc_txtDireccion.gridy = 6;
-		pnlConsultaCli.add(txtDireccion, gbc_txtDireccion);
-		
-		btnBaja = new JButton("Dar de baja");
-		btnBaja.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnBaja.setBackground(Color.RED);
-		GridBagConstraints gbc_btnBaja = new GridBagConstraints();
-		gbc_btnBaja.anchor = GridBagConstraints.WEST;
-		gbc_btnBaja.insets = new Insets(0, 0, 0, 5);
-		gbc_btnBaja.gridx = 4;
-		gbc_btnBaja.gridy = 7;
-		pnlConsultaCli.add(btnBaja, gbc_btnBaja);
 
-
+public class BajaCliente extends JPanel {
+	protected JTextField txtDni;
+	protected JTextField txtNombre;
+	protected JTextField txtApellido;
+	protected JTextField txtDireccion;
+	/**
+	 * Create the panel.
+	 */
+	public BajaCliente() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
 		JPanel pnlBajaCli = new JPanel();
 		pnlBajaCli.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		pnlBajaCli.setBackground(Color.WHITE);
@@ -144,12 +39,115 @@ import java.awt.Font;
 		gbc_pnlBajaCli.gridy = 0;
 		add(pnlBajaCli, gbc_pnlBajaCli);
 		GridBagLayout gbl_pnlBajaCli = new GridBagLayout();
-		gbl_pnlBajaCli.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
-		gbl_pnlBajaCli.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		gbl_pnlBajaCli.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
-		gbl_pnlBajaCli.rowWeights = new double[] { 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_pnlBajaCli.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_pnlBajaCli.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_pnlBajaCli.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_pnlBajaCli.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
 		pnlBajaCli.setLayout(gbl_pnlBajaCli);
-
+		
+		JLabel lblBaja = new JLabel("Baja Cliente");
+		GridBagConstraints gbc_lblBaja = new GridBagConstraints();
+		gbc_lblBaja.gridwidth = 6;
+		gbc_lblBaja.insets = new Insets(0, 0, 5, 0);
+		gbc_lblBaja.gridx = 0;
+		gbc_lblBaja.gridy = 0;
+		pnlBajaCli.add(lblBaja, gbc_lblBaja);
+		
+		JLabel lblSeleccion = new JLabel("Seleccione Cliente");
+		GridBagConstraints gbc_lblSeleccion = new GridBagConstraints();
+		gbc_lblSeleccion.gridwidth = 2;
+		gbc_lblSeleccion.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSeleccion.gridx = 1;
+		gbc_lblSeleccion.gridy = 1;
+		pnlBajaCli.add(lblSeleccion, gbc_lblSeleccion);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridwidth = 2;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.gridx = 3;
+		gbc_comboBox.gridy = 1;
+		pnlBajaCli.add(comboBox, gbc_comboBox);
+		
+		JLabel lblDni = new JLabel("Dni");
+		lblDni.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblDni = new GridBagConstraints();
+		gbc_lblDni.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDni.gridx = 2;
+		gbc_lblDni.gridy = 3;
+		pnlBajaCli.add(lblDni, gbc_lblDni);
+		
+		txtDni = new JTextField();
+		txtDni.setEditable(false);
+		txtDni.setColumns(10);
+		GridBagConstraints gbc_txtDni = new GridBagConstraints();
+		gbc_txtDni.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtDni.insets = new Insets(0, 0, 5, 5);
+		gbc_txtDni.gridx = 4;
+		gbc_txtDni.gridy = 3;
+		pnlBajaCli.add(txtDni, gbc_txtDni);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
+		gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNombre.gridx = 2;
+		gbc_lblNombre.gridy = 4;
+		pnlBajaCli.add(lblNombre, gbc_lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setEditable(false);
+		txtNombre.setColumns(10);
+		GridBagConstraints gbc_txtNombre = new GridBagConstraints();
+		gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNombre.gridx = 4;
+		gbc_txtNombre.gridy = 4;
+		pnlBajaCli.add(txtNombre, gbc_txtNombre);
+		
+		JLabel lblApellido = new JLabel("Apellidos");
+		GridBagConstraints gbc_lblApellido = new GridBagConstraints();
+		gbc_lblApellido.insets = new Insets(0, 0, 5, 5);
+		gbc_lblApellido.gridx = 2;
+		gbc_lblApellido.gridy = 5;
+		pnlBajaCli.add(lblApellido, gbc_lblApellido);
+		
+		txtApellido = new JTextField();
+		txtApellido.setEditable(false);
+		txtApellido.setColumns(10);
+		GridBagConstraints gbc_txtApellido = new GridBagConstraints();
+		gbc_txtApellido.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtApellido.insets = new Insets(0, 0, 5, 5);
+		gbc_txtApellido.gridx = 4;
+		gbc_txtApellido.gridy = 5;
+		pnlBajaCli.add(txtApellido, gbc_txtApellido);
+		
+		JLabel lblDireccion = new JLabel("Direcci\u00F3n");
+		GridBagConstraints gbc_lblDireccion = new GridBagConstraints();
+		gbc_lblDireccion.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDireccion.gridx = 2;
+		gbc_lblDireccion.gridy = 6;
+		pnlBajaCli.add(lblDireccion, gbc_lblDireccion);
+		
+		txtDireccion = new JTextField();
+		txtDireccion.setEditable(false);
+		txtDireccion.setColumns(10);
+		GridBagConstraints gbc_txtDireccion = new GridBagConstraints();
+		gbc_txtDireccion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtDireccion.insets = new Insets(0, 0, 5, 5);
+		gbc_txtDireccion.gridx = 4;
+		gbc_txtDireccion.gridy = 6;
+		pnlBajaCli.add(txtDireccion, gbc_txtDireccion);
+		
+		JButton btnBaja = new JButton("Dar de Baja");
+		btnBaja.setBackground(Color.RED);
+		GridBagConstraints gbc_btnBaja = new GridBagConstraints();
+		gbc_btnBaja.anchor = GridBagConstraints.WEST;
+		gbc_btnBaja.insets = new Insets(0, 0, 0, 5);
+		gbc_btnBaja.gridx = 4;
+		gbc_btnBaja.gridy = 7;
+		pnlBajaCli.add(btnBaja, gbc_btnBaja);
 
 	}
+
 }
--- src/vista/cliente/BusquedaCliente.java
+++ src/vista/cliente/BusquedaCliente.java
@@ -6,8 +6,16 @@ import java.awt.GridBagConstraints;
 import java.awt.Color;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
+import javax.swing.JLabel;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class BusquedaCliente extends JPanel {
+	private JTextField txtInserccion;
+	private JTextField txtNombre;
+	private JTextField txtApellidos;
+	private JTextField txtDireccion;
 
 	/**
 	 * Create the panel.
@@ -28,7 +36,100 @@ public class BusquedaCliente extends JPanel {
 		gbc_pnlBusquedaCli.gridx = 0;
 		gbc_pnlBusquedaCli.gridy = 0;
 		add(pnlBusquedaCli, gbc_pnlBusquedaCli);
-
+		GridBagLayout gbl_pnlBusquedaCli = new GridBagLayout();
+		gbl_pnlBusquedaCli.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_pnlBusquedaCli.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_pnlBusquedaCli.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_pnlBusquedaCli.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		pnlBusquedaCli.setLayout(gbl_pnlBusquedaCli);
+		
+		JLabel lblBusquedaClientes = new JLabel("Busqueda Clientes");
+		GridBagConstraints gbc_lblBusquedaClientes = new GridBagConstraints();
+		gbc_lblBusquedaClientes.gridwidth = 4;
+		gbc_lblBusquedaClientes.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBusquedaClientes.gridx = 1;
+		gbc_lblBusquedaClientes.gridy = 0;
+		pnlBusquedaCli.add(lblBusquedaClientes, gbc_lblBusquedaClientes);
+		
+		JLabel lblInserccion = new JLabel("Inserte un Dni:");
+		GridBagConstraints gbc_lblInserccion = new GridBagConstraints();
+		gbc_lblInserccion.gridwidth = 2;
+		gbc_lblInserccion.anchor = GridBagConstraints.EAST;
+		gbc_lblInserccion.insets = new Insets(0, 0, 5, 5);
+		gbc_lblInserccion.gridx = 1;
+		gbc_lblInserccion.gridy = 1;
+		pnlBusquedaCli.add(lblInserccion, gbc_lblInserccion);
+		
+		txtInserccion = new JTextField();
+		GridBagConstraints gbc_txtInserccion = new GridBagConstraints();
+		gbc_txtInserccion.insets = new Insets(0, 0, 5, 5);
+		gbc_txtInserccion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtInserccion.gridx = 3;
+		gbc_txtInserccion.gridy = 1;
+		pnlBusquedaCli.add(txtInserccion, gbc_txtInserccion);
+		txtInserccion.setColumns(10);
+		
+		JButton btnBuscar = new JButton("New button");
+		GridBagConstraints gbc_btnBuscar = new GridBagConstraints();
+		gbc_btnBuscar.anchor = GridBagConstraints.WEST;
+		gbc_btnBuscar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnBuscar.gridx = 4;
+		gbc_btnBuscar.gridy = 1;
+		pnlBusquedaCli.add(btnBuscar, gbc_btnBuscar);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
+		gbc_lblNombre.anchor = GridBagConstraints.EAST;
+		gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNombre.gridx = 1;
+		gbc_lblNombre.gridy = 2;
+		pnlBusquedaCli.add(lblNombre, gbc_lblNombre);
+		
+		txtNombre = new JTextField();
+		GridBagConstraints gbc_txtNombre = new GridBagConstraints();
+		gbc_txtNombre.gridwidth = 3;
+		gbc_txtNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNombre.gridx = 2;
+		gbc_txtNombre.gridy = 2;
+		pnlBusquedaCli.add(txtNombre, gbc_txtNombre);
+		txtNombre.setColumns(10);
+		
+		JLabel lblApellidos = new JLabel("Apellidos:");
+		GridBagConstraints gbc_lblApellidos = new GridBagConstraints();
+		gbc_lblApellidos.anchor = GridBagConstraints.EAST;
+		gbc_lblApellidos.insets = new Insets(0, 0, 5, 5);
+		gbc_lblApellidos.gridx = 1;
+		gbc_lblApellidos.gridy = 3;
+		pnlBusquedaCli.add(lblApellidos, gbc_lblApellidos);
+		
+		txtApellidos = new JTextField();
+		GridBagConstraints gbc_txtApellidos = new GridBagConstraints();
+		gbc_txtApellidos.gridwidth = 3;
+		gbc_txtApellidos.insets = new Insets(0, 0, 5, 5);
+		gbc_txtApellidos.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtApellidos.gridx = 2;
+		gbc_txtApellidos.gridy = 3;
+		pnlBusquedaCli.add(txtApellidos, gbc_txtApellidos);
+		txtApellidos.setColumns(10);
+		
+		JLabel lblDireccion = new JLabel("Direccion:");
+		GridBagConstraints gbc_lblDireccion = new GridBagConstraints();
+		gbc_lblDireccion.anchor = GridBagConstraints.EAST;
+		gbc_lblDireccion.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDireccion.gridx = 1;
+		gbc_lblDireccion.gridy = 4;
+		pnlBusquedaCli.add(lblDireccion, gbc_lblDireccion);
+		
+		txtDireccion = new JTextField();
+		GridBagConstraints gbc_txtDireccion = new GridBagConstraints();
+		gbc_txtDireccion.gridwidth = 3;
+		gbc_txtDireccion.insets = new Insets(0, 0, 5, 5);
+		gbc_txtDireccion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtDireccion.gridx = 2;
+		gbc_txtDireccion.gridy = 4;
+		pnlBusquedaCli.add(txtDireccion, gbc_txtDireccion);
+		txtDireccion.setColumns(10);
 	}
 
 }
