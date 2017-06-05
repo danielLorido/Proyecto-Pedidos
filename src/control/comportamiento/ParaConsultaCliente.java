ddffbcfeea5cca5a1fae02ebdd2dc0b003f2db6b
Ismael Bastida <ismaelbastida525@gmail.com>
2017-06-05 10:54:15 +0200

    Relleno combo v1

--- src/control/comportamiento/ParaConsultaCliente.java
+++ src/control/comportamiento/ParaConsultaCliente.java
@@ -2,17 +2,24 @@ package control.comportamiento;
 
 import vista.cliente.ConsultaCliente;
 import java.awt.event.ActionListener;
+import java.util.HashSet;
+
+import control.logica.GestorCliente;
+import modelo.Cliente;
+
 import java.awt.event.ActionEvent;
 
 public class ParaConsultaCliente extends ConsultaCliente {
 	public ParaConsultaCliente() {
 		// Tengo que rellenar el combo
 		rellenarCombo();
+		
 
 		btnBuscaCli.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Obtiene el cliente que está en el comboBox selleccionado y
 				// muestra la información en los txt
+				
 				
 				String dniCli = "", nombreCli = "", apellidoCli = "", direccionCli = "";
 				// Obtengo los valores para ese cliente
@@ -22,17 +29,18 @@ public class ParaConsultaCliente extends ConsultaCliente {
 				txtNombre.setText(nombreCli);
 				txtApellido.setText(apellidoCli);
 				txtDireccion.setText(direccionCli);
-
 			}
 		});
-
 	}
 
 	/**
 	 * Rellena el comboBox con los elementos de la lista que está en el fichero.
 	 */
 	private void rellenarCombo() {
-
+		HashSet<Cliente> lista = new GestorCliente().getListaCli();
+		for (Cliente cliente : lista) {
+			comboBox.insertItemAt(cliente, 0);
+		}
 	}
 
 }
